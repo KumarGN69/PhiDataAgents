79afd01f340779baf55789b5add2eaa58023ae5b
KumarGN69 <kumargn1969@hotmail.com>
2024-12-28 05:58:57 +0000

    changes using Ollama and local model

--- finance_ai_agent.py
+++ finance_ai_agent.py
@@ -1,7 +1,9 @@
 import openai
 from phi.agent import Agent
 from phi.model.openai import OpenAIChat
+from phi.model.ollama import Ollama
 from phi.tools.yfinance import YFinanceTools
+from langchain_ollama import OllamaLLM, ChatOllama
 from dotenv import load_dotenv
 import os
 
@@ -11,20 +13,31 @@ load_dotenv()
 # Get API key from environment
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+llm = Ollama(
+        id="llama3.2",
+        name='Ollama',
+        host= 'http://localhost:11434',
+        model="llama3.2",
+        # format="json",
+        temperature=0.0
+        )
 # Initialize the agent
 finance_agent = Agent(
     name="Finance AI Agent",
-    model=OpenAIChat(id="gpt-4o"),
+    # model=OpenAIChat(id="gpt-4o"),
+    model = llm,
     tools=[
         YFinanceTools(
-            stock_price=True,
+            stock_price=False,
             analyst_recommendations=True,
             company_info=True,
             company_news=True,
+            historical_prices=True
         )
     ],
-    instructions=["Use tables to display data"],
+    description="You are an investment analyst that researches stock prices, analyst recommendations, and stock fundamentals.",
+    instructions=["Format your response using markdown and use tables to display data where possible."],
     show_tool_calls=True,
-    markdown=True,
+    markdown=False,
 )
-finance_agent.print_response("Summarize analyst recommendations for NVDA", stream=True)
+finance_agent.print_response("Share the TSLA stock price and analyst recommendations", stream=True)
